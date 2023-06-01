30a655cd5385740e569422bdadd44f66a437e718
Carles E <carles@ganxo.net>
2023-06-01 21:02:01 +0200

    Added image and name

--- src/components/LondonWeather.jsx
+++ src/components/LondonWeather.jsx
@@ -7,15 +7,18 @@ function WeatherSearch() {
   useEffect(() => {
     const fetchWeatherData = async () => {
       const data = await searchWeather("London");
-      setWeatherData(data.weather[0].description);
+      console.log(data.weather[0]);
+      setWeatherData(data.weather[0]);
     };
 
     fetchWeatherData();
   }, []);
-
+  if (!weatherData) return null;
   return (
     <div>
-      {weatherData && <h2>{weatherData}</h2>}
+      <h2>{weatherData.main}</h2>
+      <h3>{weatherData.description}</h3>
+      <img src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}/>
     </div>
   );
 }
