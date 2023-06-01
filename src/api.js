4b589f308f9c5bf817f46063149a870f0a6d74f3
Carles E <carles@ganxo.net>
2023-06-01 20:49:06 +0200

    API Key as enviroment variable

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const searchWeather = async (query) => {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${query},uk&APPID=7227a149f860d4872de01cdafdb14e0e`
+      `https://api.openweathermap.org/data/2.5/weather?q=${query},uk&APPID=${import.meta.env.PUBLIC_API_KEY}`
     );
     const weather = await response.json();
     return weather;
@@ -8,10 +8,9 @@ export const searchWeather = async (query) => {
 
 export const LondonWeather = async () => {
     const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=7227a149f860d4872de01cdafdb14e0e"
+      `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${import.meta.env.PUBLIC_API_KEY}`
     );
     const weather = await response.json();
-    console.log(weather.weather[0].description);
     return weather.weather[0].description;
   };
 
