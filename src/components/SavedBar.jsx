ab318a6c6b21a011a53a2b1af1b6a7107cade62b
Carles Encinas <thecrax@ganxo.net>
2023-06-02 20:27:39 +0200

    Initiated react component for Save Bar

---
+++ src/components/SavedBar.jsx
@@ -0,0 +1,10 @@
+import "./SavedBar.css";
+import { state } from "../state";
+
+export default function SavedBar() {
+  return (
+    <div className="saved-bar">
+      <h1>Saved Cities</h1>
+    </div>
+  );
+}
