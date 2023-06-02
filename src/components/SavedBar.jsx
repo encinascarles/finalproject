01792b71a929fb53f6961545f8b7aa090380c711
Carles Encinas <thecrax@ganxo.net>
2023-06-02 22:43:03 +0200

    Added weather data to savedbar

--- src/components/SavedBar.jsx
+++ src/components/SavedBar.jsx
@@ -1,20 +1,38 @@
 import "./SavedBar.css";
 import { state_saved } from "../state";
 import { observer } from "mobx-react-lite";
+import { useEffect, useState } from "react";
+import { searchWeather } from "../api";
 
 function SavedCity({ city }) {
+  const [weatherData, setWeatherData] = useState("");
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await searchWeather(city);
+      setWeatherData([
+        `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+        Math.round(data.main.temp - 273.15),
+      ]);
+    };
+    fetchWeatherData();
+  }, [city]);
+  console.log(weatherData);
   return (
     <div className="saved-city">
       <h2>{city}</h2>
+      <img src={weatherData[0]} />
+      <h4>Temp: {weatherData[1]} °C</h4>
       <button onClick={() => state_saved.rmSaved(city)}>Delete</button>
+
+      {/*<h4>Temp: {Math.round(weatherData.main.temp-273.15)} °C</h4>*/}
     </div>
   );
 }
 
-
 function SavedBar() {
-    const savedCities = state_saved.getSaved();
-    //const savedCities = ["London", "Paris", "New York"];
+  const savedCities = state_saved.getSaved();
+  //const savedCities = ["London", "Paris", "New York"];
   return (
     <div className="saved-bar">
       <h1>Saved Cities</h1>
@@ -24,6 +42,5 @@ function SavedBar() {
     </div>
   );
 }
-
 
 export default observer(SavedBar);
