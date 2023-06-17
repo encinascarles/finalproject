0fbe21e0339e16970495dbaa75285e375db138f4
Carles Encinas <thecrax@ganxo.net>
2023-06-17 18:14:44 +0200

    Change only so that vercel updates itself

--- src/api.js
+++ src/api.js
@@ -3,8 +3,10 @@ export const searchWeather = async (query) => {
       `https://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=${import.meta.env.PUBLIC_API_KEY}`
     );
     const weather = await response.json();
+    console.log(import.meta.env.PUBLIC_API_KEY);
     return weather;
 };
+
 
 export const LondonWeather = async () => {
     const response = await fetch(
